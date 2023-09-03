affc7dd777367324011d19ef588a9709ae36452d
Pranav V <pranav9708@gmail.com>
2023-09-03 22:10:05 +0530

    complete user signUp

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,36 +1,46 @@
-module.exports.signinUser=(req,res)=>{
+const User = require('../models/User');
+
+module.exports.signinUser = (req, res) => {
+    const {email,password}=req.body;
     
-
     return res.status(200).json(req.body);
 }
 
-module.exports.signupUser=(req,res)=>{
-    const {email,password,confirmPassword}=req.body;
-    console.log(email,password,confirmPassword);
-    if(password.length<8){
-        req.flash('error','password must be atleast 8 characters long');
-        return res.redirect('/signup');
+module.exports.signupUser = async (req, res) => {
+    try {
+        const { email, password, confirmPassword } = req.body;
+        console.log(email, password, confirmPassword);
+        if (password.length < 8) {
+            req.flash('error', 'password must be atleast 8 characters long');
+            return res.redirect('/signup');
+        }
+
+        const specialCharacters = /[`~!@#$%^&*()_+={}[\]:;'"<>?,.\\-]/;
+        if (!specialCharacters.test(password)) {
+            req.flash('error', 'Password must contain atleast one special character');
+            return res.redirect('/signup');
+        }
+
+        if (password !== confirmPassword) {
+            req.flash('error', 'passwords does not match');
+            return res.redirect('/signup');
+
+        }
+        await User.create(req.body);
+        return res.render('home',{
+            user:email.split('@')[0],
+        })
+
+    } catch (err) {
+        return res.status(403).json(err);
     }
 
-    const specialCharacters=/[`~!@#$%^&*()_+={}[\]:;'"<>?,.\\-]/;
-    if(!specialCharacters.test(password)){
-        req.flash('error','Password must contain atleast one special character');
-        return res.redirect('/signup');
-    }
-
-    if(password!==confirmPassword){
-        req.flash('error','passwords does not match');
-        return res.redirect('/signup');
-
-    }
-
-    return res.status(200).json(req.body);
 }
 
-module.exports.renderSignIn=(req,res)=>{
+module.exports.renderSignIn = (req, res) => {
     return res.render('signin');
 }
 
-module.exports.renderSignUP=(req,res)=>{
+module.exports.renderSignUP = (req, res) => {
     return res.render('signUp');
 }
